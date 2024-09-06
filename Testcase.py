ca16b0e6f60ca4b06236932a33e2b118bbae8ef7
brianyu1986 <47875126+brianyu1986@users.noreply.github.com>
2024-09-06 21:49:46 +0800

    Update Testcase.py

--- Testcase.py
+++ Testcase.py
@@ -1,1 +1,30 @@
+import pytest
+import TestFunction
 
+
+@pytest.mark.string
+def test_case():    # TestCase use to check Status 
+    assert TestFunction.Callapi("https://swapi.dev/api/films/", "POST") == {'detail': "Method 'POST' not allowed."}  # Case 1
+    assert TestFunction.Callapi("https://swapi.dev/api/films/7") == {'detail': 'Not found'}                          # Case 4
+    assert TestFunction.Callapi("https://swapi.dev/api/films/a") == {'detail': 'Not found'}                          # Case 5
+
+
+def Case2():
+    url = "https://swapi.dev/api/films/"
+    response = requests.request("get", url)
+    jsonResult = json.loads(response.text)
+    print(jsonResult)
+
+
+def Case3(url):
+    uurl = "https://swapi.dev/api/films/1/"
+    response = requests.request("get", url)
+    jsonResult = json.loads(response.text)
+    print(jsonResult)
+
+
+def Case6():
+    url = "https://swapi.dev/api/films/1/"
+    response = requests.request("get", url)
+    jsonResult = json.loads(response.text)
+    print(jsonResult["vehicles"])
